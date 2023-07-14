b48d5364e35e6d3ade8ff1739dfc4555e4ae9798
notUnEeVeN <tybalt.mallet@gmail.com>
2023-07-14 12:16:31 -0700

    properly linked javascript and css files, added the like date/time to the top of the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+$(document).ready(function(){
+  setInterval(function() {
+    $('#currentDay').text(dayjs().format('MMM D, YYYY hh:mm:ss A'))
+  }, 1000);
+});
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
