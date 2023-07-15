6caf62a258b8ca6cb7fdf1df1da64bfa9e0942a3
notUnEeVeN <tybalt.mallet@gmail.com>
2023-07-15 11:12:21 -0700

    added the readme file, fixed the bug whee the rows still didn't update automatically when the hour changed, everything is now functional

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,8 +13,8 @@ $(document).ready(function(){
           localStorage.setItem(ID, description);
         }
       });
-    //added a setinterval function, so that when the time updates, the colors of the rows will also live update
 
+    //added a setinterval function, so that when the time updates, the colors of the rows will also live update
     //here i used the jquery each method, that iterates through every element with the class '.time-block' and then executes the function 
     //added a setinterval function so that the classes will refresh every second, and will in turn live update when the hour changes 
     setInterval(function() {
@@ -22,7 +22,9 @@ $(document).ready(function(){
         //setting our variables , we are fetching the ID attribute, which I set equivalent to the time they represent using the 24 hour clock
         var hour = $(this).attr('id');
         //getting the current hour using dayjs and setting it to a variable
-        var currentHour = dayjs().hour();
+        var currentHour = dayjs().hour()
+        //added this line to remove all the classes before adding new ones, as an attempt to fix the bug where the classes dont live update when the hour changes
+        $(this).removeClass('past present future');
         //for each row in our planner, we check the current hour (which also comes in the 24 hour format) against our ID's, that i set to the appropriate 24 hour number, and then set the appropriate class depending on their relation to the current time 
         if(currentHour > hour) {
           $(this).addClass('past');
